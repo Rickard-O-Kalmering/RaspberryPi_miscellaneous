5b79f02866f44b23829e7cf1f446a1ad6bcc76db
Rickard <Rickard@Rickard-Dator>
2014-12-27 22:14:51 +0100

    WiringPi test

---
+++ Raspberry Pi (C++)/Blink.c
@@ -0,0 +1,22 @@
+/*
+ * Blink.c
+ *
+ *  Created on: 27 dec 2014
+ *      Author: Rickard
+ */
+#include <wiringPi>
+
+void main(){
+
+wiringPiSetup();
+pinMode(0, OUTPUT);
+
+	for(;;){
+		digitalWrite(0, HIGH);
+		delay(500);
+		digitalWrite(0, LOW);
+		delay(500);
+	}
+}
+
+
